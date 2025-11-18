7f32677db407cbfd69cd6ece321c7220d536bf4b
Chris <git@ccox.dev>
2025-11-17 21:19:05 -0500

    Add 1Password application and set vivaldi and zen browsers to work with it

---
+++ system/programs/1password.nix
@@ -0,0 +1,21 @@
+{
+  ...
+}:
+
+{
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = [ "chris" ];
+  };
+
+  environment.etc = {
+    "1password/custom_allowed_browsers" = {
+      text = ''
+        vivaldi-bin
+        zen
+      '';
+      mode = "0755";
+    };
+  };
+}
