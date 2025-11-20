7de40f3db26de0f737a5c9679ddb93665e746621
Chris <git@ccox.dev>
2025-11-20 04:29:58 -0500

    Update `nushell` config to automatically call `fastfetch` when not in nix shell. Allow `nushell` to still be used instead of `bash` inside nix shells.

--- home/programs/terminal/nushell.nix
+++ home/programs/terminal/nushell.nix
@@ -1,8 +1,13 @@
 {
+  config,
+  pkgs,
   ...
 }:
 
 {
+  home.file."${config.xdg.configHome}/nushell/nix-your-shell.nu".source =
+    pkgs.nix-your-shell.generate-config "nu";
+
   programs = {
     nushell = {
       enable = true;
@@ -10,12 +15,23 @@
       # configFile.source = ./.../config.nu;
       # for editing directly to config.nu
       extraConfig = ''
+
+        # Only shows `fastfetch` if interactive terminal and not inside nix shell
+        if ($nu.is-interactive == true) and ("IN_NIX_SHELL" not-in $env) {
+          fastfetch
+        }
+
         # Common ls aliases and sort them by type and then name
         # Inspired by https://github.com/nushell/nushell/issues/7190
         def lla [...args] { ls -la ...(if $args == [] {["."]} else {$args}) | sort-by type name -i }
         def la  [...args] { ls -a  ...(if $args == [] {["."]} else {$args}) | sort-by type name -i }
         def ll  [...args] { ls -l  ...(if $args == [] {["."]} else {$args}) | sort-by type name -i }
         def l   [...args] { ls     ...(if $args == [] {["."]} else {$args}) | sort-by type name -i }
+
+        $env.config.show_banner = false
+
+        # Sets `nix` to use nushell instead of bash
+        source nix-your-shell.nu
 
         use ~/.cache/starship/init.nu
       '';
