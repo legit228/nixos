3e0e70a021f6d0906a28d459d224ad2b1b825bd2
Christopher Knight <git@ccox.dev>
2026-01-03 00:22:22 -0500

    Remove `1Password` GUI application since it is being installed alongside `niri` already.

--- system/programs/1password.nix
+++ system/programs/1password.nix
@@ -3,11 +3,11 @@
 }:
 
 {
-  programs._1password.enable = true;
-  programs._1password-gui = {
-    enable = true;
-    polkitPolicyOwners = [ "chris" ];
-  };
+  programs._1password-cli.enable = true;
+  # programs._1password-gui = {
+  #   enable = true;
+  #   polkitPolicyOwners = [ "chris" ];
+  # };
 
   environment.etc = {
     "1password/custom_allowed_browsers" = {
