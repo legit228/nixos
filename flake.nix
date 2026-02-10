8b52c8478fb570c8ca48a855017d68a3e6d719c7
Christopher Knight <git@ccox.dev>
2026-02-10 00:38:14 -0500

    Add several new inputs and rework Star Citizen module.

--- flake.nix
+++ flake.nix
@@ -14,6 +14,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    disko = {
+      url = "github:nix-community/disko/latest";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     matugen.url = "github:InioX/matugen";
 
     niri.url = "github:sodiboo/niri-flake";
@@ -33,6 +38,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nix-minecraft.url = "github:Infinidoge/nix-minecraft";
+
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -42,6 +49,10 @@
       url = "github:moonlight-mod/moonlight";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    bakkesmod-nix.url = "github:AddG0/bakkesmod-nix";
+
+    xremap-flake.url = "github:xremap/nix-flake";
 
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
 
@@ -70,7 +81,33 @@
           inputs.home-manager.nixosModules.default
           # inputs.agenix.nixosModules.default
           chaotic.nixosModules.default
-          inputs.nix-citizen.nixosModules.StarCitizen
+          inputs.disko.nixosModules.disko
+
+          nix-citizen.nixosModules.default
+          {
+            programs.rsi-launcher = {
+              # Enables the star citizen module
+              enable = true;
+              umu.enable = true;
+              gamescope = {
+                enable = true;
+                args = [ "-f" "-w 3840" "-h 1600" "-W 3840" "-H 1600" "--force-grab-cursor" ];
+              };
+
+              # Additional commands before the game starts
+              preCommands = ''
+                export DXVK_HUD=compiler;
+                export MANGOHUD=1;
+              '';
+              # # This option is enabled by default
+              # #  Configures your system to meet some of the requirements to run star-citizen
+              # # Set `vm.max_map_count` default to `16777216` (sysctl(8))
+              # #Set `fs.file-max` default to `524288` (sysctl(8))
+              # #Also sets `security.pam.loginLimits` to increase hard (limits.conf(5))
+              # # Changes outlined in  https://github.com/starcitizen-lug/knowledge-base/wiki/Manual-Installation#prerequisites
+              # setLimits = false;
+            };
+          }
         ];
       };
     };
